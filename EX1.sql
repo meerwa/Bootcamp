723117ff9aab5c1ce73efee2cffd781814f50e4e
meerwa <meerwa@example.com>
2025-03-17 14:42:00 +0000

    Commiting week2

---
+++ EX1.sql
@@ -0,0 +1,22 @@
+/*create table customers(
+id_customer int Primary key,
+first_name varchar(50) NOT NULL,
+last_name varchar(50)  NOT NULL
+)
+
+
+
+INSERT INTO customers (id_customer, first_name, last_name) VALUES 
+(1, 'Greg', 'Jones'),
+(2, 'Sandra', 'Jones'),
+(3, 'Scott', 'Scott'),
+(4, 'Trevor', 'Green'),
+(5, 'Melanie', 'Johnson');*/
+
+--select * from customers
+--select * from items
+--select * from items where price>80
+--select * from items where price<300
+--select * from customers where last_name='Smith'
+--select * from customers where first_name='Jones'
+--select * from customers where first_name != 'Scott'
